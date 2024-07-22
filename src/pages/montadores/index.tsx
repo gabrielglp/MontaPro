794b01e3da0ca1bba7bb47f1b18f33e26beaf345
gabrielglp <gabriel_pg08@hotmail.com>
2024-07-22 14:57:33 -0300

    style: responsive page montadores

--- src/pages/montadores/index.tsx
+++ src/pages/montadores/index.tsx
@@ -56,27 +56,27 @@ export default function dashBoard() {
         <main className="">
             <NavBar/>
             <div className="mt-8 flex justify-center items-center flex-col">
-                <div className="bg-white flex rounded-full pr-20 pl-2 py-2 mb-10">
-                    <input className="px-4 mr-4 w-[41rem] bg-gray-050 flex rounded-full" placeholder="Digite o seu Munícipio"/>
+                <div className="bg-white flex rounded-full pr-4 md:pr-20 pl-2 py-2 mb-10">
+                    <input className="px-4 mr-4 w-11/12 md:w-[36rem] bg-gray-050 flex rounded-full" placeholder="Digite o seu Munícipio"/>
                     <button className="rounded-3xl">
                         <Image className="w-10"  src={Search} alt=""/>
                     </button>
                 </div>
 
                 {freelas.map(freela => (
-                    <div key={freela.id} className="flex justify-between mb-4 p-6 bg-white text-gray-700 w-[50.5rem] items-center rounded-3xl">
+                    <div key={freela.id} className="flex justify-between mb-4 p-4 md:p-6 bg-white text-gray-700 w-11/12 md:w-[45rem] items-center rounded-3xl">
                         <div className="flex items-center">
                             <div className="mr-4">
-                                <Image className="w-20 h-20 rounded-full bg-cover bg-center bg-no-repeat"  src={Default} alt=""/>
+                                <Image className="w-14 md:w-20 h-14 md:h-20 rounded-full bg-cover bg-center bg-no-repeat"  src={Default} alt=""/>
                             </div>
                             <div className="flex flex-col">
-                                <p className="text-xl font-bold">{freela.name}</p>
-                                <p className="flex mt-4 items-center font-medium"><Image className="mr-2 w-4 h-4" src={Phone} alt=""/>{freela.phoneNumber}</p>
-                                <p className="flex items-center font-medium"><Image className="mr-2 w-4 h-4" src={Location} alt=""/>{freela.county.name}</p>
+                                <p className="text-sm md:text-xl font-bold">{freela.name}</p>
+                                <p className="text-sm md:text-sm flex mt-4 items-center font-medium"><Image className="mr-2 w-4 h-4" src={Phone} alt=""/>{freela.phoneNumber}</p>
+                                <p className="text-sm md:text-sm flex items-center font-medium"><Image className="mr-2 w-4 h-4" src={Location} alt=""/>{freela.county.name}</p>
                             </div>
                         </div>
                         <Link href={`https://api.whatsapp.com/send/?phone=${freela.phoneNumber}&text=Olá,%20tudo%20bem?&type=phone_number&app_absent=0`}>
-                            <Image className="w-20 h-20"  src={WhatsApp} alt=""/>
+                            <Image className="w-14 md:w-20 h-14 md:h-20"  src={WhatsApp} alt=""/>
                         </Link>
                     </div>
                 ))}
